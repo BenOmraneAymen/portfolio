a4d96dbde5e71131293d8db265c2c38da23487ce
Aymen Ben Omrane <aymen.b.omrane@gmail.com>
2023-11-23 23:45:11 +0100

    fixed visual bug

--- src/components/imageContainer.jsx
+++ src/components/imageContainer.jsx
@@ -3,7 +3,7 @@ export default function ImageContainer({image,text}){
 return(
     <div className="flex flex-col items-center my-2 mx-1" >
         <img src={image} alt="" className="w-full rounded-t-lg " />
-        <p>{text}</p>
+        <p className="text-center" >{text}</p>
     </div>
 )
 }
